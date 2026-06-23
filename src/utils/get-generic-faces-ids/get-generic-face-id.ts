7d9d30b3892c51b80dc70016f5136b65d7190c3d
ViniMacacari <macacarivinicius@gmail.com>
2026-06-23 10:01:18 -0300

    feat: generic face id getter added

---
+++ src/utils/get-generic-faces-ids/get-generic-face-id.ts
@@ -0,0 +1,15 @@
+interface FaceInformation {
+    id: number
+    region: 'caucasian' | 'latin' | 'asian' | 'african'
+}
+
+export const genericIds: FaceInformation[] = [
+    {
+        id: 39,
+        region: 'caucasian'
+    },
+    {
+        id: 3515,
+        region: 'caucasian'
+    }
+]
